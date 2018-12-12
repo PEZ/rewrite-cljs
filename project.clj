3d55dcfd974acb5aa83eb0f238a45c461e41d245
Peter Strömberg <pez@pezius.com>
2018-12-12 10:26:15 +0100

    For calva-lib to use pez/rewrite-cljs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rewrite-cljs "0.4.4"
+(defproject pez/rewrite-cljs "0.4.4-SNAPSHOT"
   :description "Comment-/Whitespace-preserving rewriting of EDN documents."
   :url "https://github.com/rundis/rewrite-cljs"
   :license {:name "MIT License"
@@ -14,8 +14,7 @@
              {:plugins [[lein-cljsbuild "1.1.2"]
                         [lein-doo "0.1.6"]]
 
-              :cljsbuild {
-                          :builds {:test
+              :cljsbuild {:builds {:test
                                    {:source-paths ["src" "test"]
                                     ;:notify-command ["phantomjs" :cljs.test/runner "target/out/unit-test.js"]
                                     :compiler {:output-to "target/out/unit-test.js"
@@ -23,11 +22,11 @@
                                                :optimizations :whitespace
                                                :pretty-print true}}}}}
 
-           :doc {:plugins  [[funcool/codeina "0.1.0"
-                             :exclusions [org.clojure/clojure]]]
-                 :codeina {:sources ["src"]
-                           :language :clojurescript
-                           :src-dir-uri "https://github.com/rundis/rewrite-cljs/blob/master/"
-                           :src-linenum-anchor-prefix "L"}}}
+             :doc {:plugins  [[funcool/codeina "0.1.0"
+                               :exclusions [org.clojure/clojure]]]
+                   :codeina {:sources ["src"]
+                             :language :clojurescript
+                             :src-dir-uri "https://github.com/rundis/rewrite-cljs/blob/master/"
+                             :src-linenum-anchor-prefix "L"}}}
 
   :aliases {"auto-test" ["with-profile" "dev" "do" "clean," "cljsbuild" "auto" "test"]})
